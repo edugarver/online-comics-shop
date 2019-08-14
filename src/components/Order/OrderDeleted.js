9d5087e864f7e0d5458ad9109b2c40e31dc077ab
edugarver <edugarver@gmail.com>
2019-08-14 19:03:20 +0200

    quick fix

--- src/components/Order/OrderDeleted.js
+++ src/components/Order/OrderDeleted.js
@@ -4,7 +4,7 @@ export default function OrderDeleted() {
   return (
     <div className="container mt-5">
       <div className="row">
-        <div className="col-10 mx-auto text-center text-title">
+        <div className="col-10 mx-auto text-center text-title order-deleted">
           <h1>The order was successfully deleted</h1>
         </div>
       </div>
